9ed5a887ab412f06c511313f51d6f6249d711d35
SpeakSoftly <speaksoftly@SpeakSofylt.local>
2020-05-13 02:19:58 +0800

    完成登陆退出功能

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <el-button type="info" @click="logout">退出</el-button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  methods: {
+    logout() {
+      // 清空客户端 token
+      window.sessionStorage.clear()
+      // 重定向到登陆页
+      this.$router.push('/login')
+    }
   }
 }
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped></style>
