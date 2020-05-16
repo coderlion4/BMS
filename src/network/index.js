c39b400d41316a4280c9afc55f282333d5a77d76
SpeakSoftly <speaksoftly@SpeakSofylt.local>
2020-05-16 15:41:47 +0800

    项目开发完成

--- src/network/index.js
+++ src/network/index.js
@@ -1,10 +1,18 @@
 import Vue from 'vue'
 import axios from 'axios'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
 
 axios.interceptors.request.use(config => {
   config.headers.Authorization = window.sessionStorage.getItem('token')
+  NProgress.start()
+  return config
+})
+
+axios.interceptors.response.use(config => {
+  NProgress.done()
   return config
 })
 
